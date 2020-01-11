e7f4a713d890d3e99a09e1bca476c20623dbf708
kof-f <kofif7kofi@gmail.com>
2020-01-11 18:26:33 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,54 @@
+// core modules --> npm packages --> local files
+const yargs = require('yargs') 
+const chalk = require('chalk') 
+const notes = require('./notes.js') 
+
+// Customize yargs version
+yargs.version('2.0.0') 
+
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: (argv) => { notes.addNote(argv.title, argv.body) }
+}) 
+
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: (argv) => { notes.removeNote(argv.title) }
+}) 
+
+yargs.command({
+    command: 'list',
+    describe: 'Listing a note',
+    handler: () => { console.log('Listing the current notes.') }
+}) 
+
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: () => { console.log('Reading the note!') }
+}) 
+
+yargs.parse() 
---
+++ notes.js
@@ -0,0 +1,57 @@
+const fs = require('fs') 
+const chalk = require('chalk')
+
+const getNotes = function (){
+    return 'Your notes...'
+}
+
+const addNote = (title, body) => {
+    const notes = loadNotes() 
+    const duplicateNotes = notes.filter((note) => { return note.title === title })
+
+    if (duplicateNotes.length === 0){
+        notes.push({
+            title: title,
+            body: body
+        }) 
+        saveNotes(notes) 
+        console.log(chalk.green('New note added!'))
+    } else {
+        console.log(chalk.red('Note title taken!')) 
+    }
+}
+
+const removeNote = (title) => {
+    const notes = loadNotes() 
+    const remainingNotes = notes.filter((note) => { return note.title !== title })
+    console.log(remainingNotes)
+
+    if (remainingNotes.length === notes.length){
+        console.log(chalk.red('That note does not exist! Please check the title.'))
+    } else {
+        saveNotes(remainingNotes)    
+        console.log(chalk.green('The note ' + chalk.bold(title) + ' has been removed!'))
+    }
+}
+
+const saveNotes = (notes) => {
+    const dataJSON = JSON.stringify(notes) 
+    fs.writeFileSync('notes.json', dataJSON) 
+}
+
+const loadNotes = () => {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json') 
+        const dataJSON = dataBuffer.toString() 
+        return JSON.parse(dataJSON) 
+    } catch (e) {
+        return [] 
+    }
+
+}
+
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote,
+    removeNote: removeNote
+} 
